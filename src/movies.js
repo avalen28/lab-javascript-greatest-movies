c9475abd59516ae069f016ba01730fd44b3b7e8a
Alberto Valenzuela <avalen28@gmail.com>
2022-11-11 14:39:24 +0100

    lab started. iterations methods selected

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,29 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  const directorsArr = moviesArray.map((element) => element.director);
+  console.log(directorsArr);
+  return directorsArr;
+} // fem un neus 7
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {} //filter spilberg && drama, luego el length
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {} // map element.score      reduce((total,currentValue) => total + currentValue, 0) /array.length  "result.toFixed(2)"
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {} // filter de dramas. invoke la funcion anterior()
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {} // sort
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {} // sort.title y luego slice [0,20]
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {} //map duration split(" ")  replace (h y min)
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
